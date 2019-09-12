01f5b8bfd890d7b4211ba2e23fa3e338d3c9c3fb
Kevin Wenger <wenger.kev@gmail.com>
2019-09-12 18:56:18 +0200

    add basic tests to ensure chain resolver works as designed

---
+++ tests/src/Kernel/DefaultShopResolverTest.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Drupal\Tests\commerce_trustedshops\Kernel;
+
+use Drupal\commerce_trustedshops\Context;
+use Drupal\Core\Language\Language;
+use Drupal\Core\Language\LanguageInterface;
+use Drupal\Tests\commerce\Kernel\CommerceKernelTestBase as DrupalCommerceKernelTestBase;
+use Drupal\language\Entity\ConfigurableLanguage;
+
+/**
+ * @coversDefaultClass \Drupal\commerce_trustedshops\Resolver\DefaultShopResolver
+ *
+ * @group commerce_trustedshops
+ */
+class DefaultShopResolverTest extends DrupalCommerceKernelTestBase {
+
+  /**
+   * The Entity Type Manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The Trusted Shops used for test.
+   *
+   * @var \Drupal\commerce_trustedshops\Entity\ShopInterface[]
+   */
+  protected $testShops;
+
+  /**
+   * The stores used for test.
+   *
+   * @var \Drupal\commerce_store\Entity\StoreInterface[]
+   */
+  protected $testStores;
+
+  /**
+   * The default Trusted Shop resolver.
+   *
+   * @var \Drupal\commerce_trustedshops\Resolver\DefaultShopResolver
+   */
+  protected $defaultShopResolver;
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = [
+    'locale',
+    'language',
+    'content_translation',
+    'commerce_store',
+    'commerce_trustedshops',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->installEntitySchema('commerce_trustedshops_shop');
+    $this->container->get('content_translation.manager')
+      ->setEnabled('commerce_trustedshops_shop', 'tsid', TRUE);
+
+    // Add French as second lang. English is always added by default.
+    ConfigurableLanguage::createFromLangcode('fr')->save();
+
+    $this->defaultShopResolver = $this->container->get('commerce_trustedshops.default_shop_resolver');
+
+    $this->testStores[] = $this->entityTypeManager->getStorage('commerce_store')->load(1);
+    $store_2 = $this->entityTypeManager
+      ->getStorage('commerce_store')->create([
+        'type' => 'online',
+        'name' => $this->randomString(8),
+        'mail' => $this->randomString(16),
+      ]);
+    $store_2->save();
+    $this->testStores[] = $store_2;
+
+    $ts_1 = $this->entityTypeManager
+      ->getStorage('commerce_trustedshops_shop')->create([
+        'tsid' => 'R9H9J8ETR1PBFYZ1564ODXCII6EU9ZRNRFI8SC',
+        'langcode' => 'en',
+        'store' => 1,
+      ]);
+    $ts_1->addTranslation('fr', [
+      'tsid' => 'Z8F8A8ETZ1PBFiZ1223ODXCPP6EU8ZRNRFP8SC',
+    ]);
+    $ts_1->save();
+    $this->testShops[$ts_1->id()] = $ts_1;
+
+    $ts_2 = $this->entityTypeManager
+      ->getStorage('commerce_trustedshops_shop')->create([
+        'tsid' => 'EO2EDIY26B5JB3T50ICP0XZM4BJ3SNSK73DZ6BO',
+        'langcode' => 'fr',
+        'store' => 2,
+      ]);
+    $ts_2->save();
+    $this->testShops[$ts_2->id()] = $ts_2;
+  }
+
+  /**
+   * @covers \Drupal\commerce_trustedshops\Resolver\DefaultShopResolver::resolve
+   *
+   * @dataProvider providerDefaultShopAndLanguage
+   */
+  public function testResolve($store_id, LanguageInterface $language, $expected_shop_id, $expected_tsid) {
+    $store_storage = $this->entityTypeManager->getStorage('commerce_store');
+    $shop_storage = $this->entityTypeManager->getStorage('commerce_trustedshops_shop');
+
+    $store = $store_storage->load($store_id);
+    $context = new Context($store, $language);
+
+    $shop = $this->defaultShopResolver->resolve($context);
+    $expected_shop = $shop_storage->load($expected_shop_id);
+
+    $this->assertEqual($shop->id(), $expected_shop->id());
+    $this->assertEqual($shop->get('tsid')->value, $expected_tsid);
+  }
+
+  /**
+   * Every potential Store & Language variations.
+   *
+   * @return array
+   *   An array of each store/language variation.
+   */
+  public function providerDefaultShopAndLanguage() {
+    return [
+      [1, new Language(['id' => 'en']), 1,
+        'R9H9J8ETR1PBFYZ1564ODXCII6EU9ZRNRFI8SC',
+      ],
+      [1, new Language(['id' => 'fr']), 1,
+        'Z8F8A8ETZ1PBFiZ1223ODXCPP6EU8ZRNRFP8SC',
+      ],
+      [2, new Language(['id' => 'fr']), 2,
+        'EO2EDIY26B5JB3T50ICP0XZM4BJ3SNSK73DZ6BO',
+      ],
+    ];
+  }
+
+  /**
+   * @covers \Drupal\commerce_trustedshops\Resolver\DefaultShopResolver::resolve
+   */
+  public function testResolveNull() {
+    $context = new Context($this->testStores[1], new Language(['id' => 'en']));
+    $shop = $this->defaultShopResolver->resolve($context);
+    $this->assertNull($shop);
+  }
+
+}
---
+++ tests/src/Unit/Resolver/ChainShopResolverTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Drupal\Tests\commerce_trustedshops\Unit\Resolver;
+
+use Drupal\commerce_trustedshops\Resolver\ChainShopResolver;
+use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * @coversDefaultClass \Drupal\commerce_trustedshops\Resolver\ChainShopResolver
+ *
+ * @group commerce_trustedshops
+ */
+class ChainShopResolverTest extends UnitTestCase {
+
+  /**
+   * The resolver.
+   *
+   * @var \Drupal\commerce_trustedshops\Resolver\ChainShopResolver
+   */
+  protected $resolver;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    parent::setUp();
+    $this->resolver = new ChainShopResolver();
+  }
+
+  /**
+   * Tests the resolver and priority.
+   *
+   * ::covers addResolver
+   * ::covers getResolvers
+   * ::covers resolve.
+   */
+  public function testResolver() {
+    $container = new ContainerBuilder();
+
+    $mock_builder = $this->getMockBuilder('Drupal\commerce_trustedshops\Resolver\ShopResolverInterface')
+      ->disableOriginalConstructor();
+
+    $first_resolver = $mock_builder->getMock();
+    $first_resolver->expects($this->once())
+      ->method('resolve');
+    $container->set('commerce.first_resolver', $first_resolver);
+
+    $second_resolver = $mock_builder->getMock();
+    $second_resolver->expects($this->once())
+      ->method('resolve')
+      ->willReturn('testShop');
+    $container->set('commerce.second_resolver', $second_resolver);
+
+    $third_resolver = $mock_builder->getMock();
+    $third_resolver->expects($this->never())
+      ->method('resolve');
+    $container->set('commerce.third_resolver', $third_resolver);
+
+    // Mimic how the container would add the services.
+    // @see \Drupal\Core\DependencyInjection\Compiler\TaggedHandlersPass::process
+    $resolvers = [
+      'commerce.first_resolver' => 900,
+      'commerce.second_resolver' => 400,
+      'commerce.third_resolver' => -100,
+    ];
+    arsort($resolvers, SORT_NUMERIC);
+    foreach ($resolvers as $id => $priority) {
+      $this->resolver->addResolver($container->get($id));
+    }
+
+    $result = $this->resolver->resolve();
+    $this->assertEquals('testShop', $result);
+  }
+
+}
